a5ee98508914904cd0d12a7c0db3eef711e639b8
Jusouza <jujussouza19@gmail.com>
2025-09-27 15:00:53 -0300

    creating new service calls and listing new categories

--- app/src/main/java/com/devspacecinenow/data/model/MovieDto.kt
+++ app/src/main/java/com/devspacecinenow/data/model/MovieDto.kt
@@ -1,12 +1,15 @@
 package com.devspacecinenow.data.model
 
-import kotlinx.serialization.SerialName
-
+import com.google.gson.annotations.SerializedName
 
 data class MovieDto(
     val id: Int,
     val title: String,
     val overview: String,
-    @SerialName("poster_path")
-    val postPath: String
-)
+    @SerializedName("poster_path")
+    val posterPath: String?
+){
+    val posterFullPath: String
+        get() = "https://image.tmdb.org/t/p/w300$posterPath"
+    //from TMDB/guides/basics to catch the URL
+}
