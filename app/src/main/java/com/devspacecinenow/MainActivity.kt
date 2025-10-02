d015f31f600ebbdefb9e7b733e7c6cea29d98211
Jusouza <jujussouza19@gmail.com>
2025-10-02 16:22:37 -0300

    Adding view model calls and creating viewmodels

---
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.devspacecinenow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.devspacecinenow.detail.data.DetailService
+import com.devspacecinenow.detail.presentation.MovieDetailViewModel
+import com.devspacecinenow.list.presentation.MovieListViewModel
+import com.devspacecinenow.presentation.theme.CineNowTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val listViewModel by viewModels<MovieListViewModel>{ MovieListViewModel.Factory }
+    private val detailViewModel by viewModels<MovieDetailViewModel>{ MovieDetailViewModel.Factory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CineNowTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.Companion.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CineNowApp(
+                        listViewModel = listViewModel,
+                        detailViewModel = detailViewModel
+                    )
+                }
+            }
+        }
+    }
+}
